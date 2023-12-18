647e83596482dff6c72397519096ccfe73cf7306
Isa <git@isabellnoack.com>
2023-12-18 12:14:27 +0100

    RecyclerView - Klassen erstellt
    RecyclerViewItem
    RecyclerViewAdapter
    erstellt

---
+++ app/src/main/java/com/isabellnoack/myapp/RecyclerViewAdapter.java
@@ -0,0 +1,44 @@
+package com.isabellnoack.myapp;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder> {
+
+    private ArrayList<RecyclerViewItem> recyclerViewItems; //ArrayList<typ> variablen-name
+
+    //für was wird das verwendet?
+    private Context context;
+
+    @NonNull
+    @Override
+
+    public RecyclerViewAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.recyclerview_item,parent,false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerViewAdapter.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return recyclerViewItems.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+}
---
+++ app/src/main/java/com/isabellnoack/myapp/RecyclerViewItem.java
@@ -0,0 +1,11 @@
+package com.isabellnoack.myapp;
+
+import android.graphics.Bitmap;
+
+public class RecyclerViewItem {
+
+    private String name;
+    private Integer ID;
+    private Bitmap image;
+
+}
